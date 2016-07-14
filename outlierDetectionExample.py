60dd236594fb58cf7a0405361ce3a9220a31b280
justinstuck <justin.stuck@incontextsolutions.com>
2016-07-14 16:43:04 -0500

    "Bad" 2D Decision Boundary
    Added decision boundary for differentiating "bad" outliers from good
    ones. This is defined where the normalized latency values are greater
    than the normed bandwidth values for outliers

--- outlierDetectionExample.py
+++ outlierDetectionExample.py
@@ -17,7 +17,7 @@ from scipy import stats
 
 from sklearn import svm
 from sklearn.covariance import EllipticEnvelope
-
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn import preprocessing
 
 
@@ -29,6 +29,7 @@ def get_data():
     #'4BC154FD-6429-444E-B589-4B3B7CF1BDA6'
     dqs = pd.read_csv("C:/Users/Justin.Stuck/Desktop/JDQs.csv",low_memory=False)['ticketId']
     history = pd.concat([history[history.name =='Unity Frame Shopping'],history[history.name =='Unity Frame Tutorial']])
+    rawShopHistory = history[history.name =='Unity Frame Shopping']
     history['latency'] = preprocessing.scale(history['latency'].apply(lambda x: math.log(x+.0000000000001)))
     history['bandwidth'] = preprocessing.scale(history['bandwidth'].apply(lambda x: math.log(x+.0000000000001)))
     history['framerate'] = preprocessing.scale(history['framerate'])
@@ -39,7 +40,7 @@ def get_data():
     history = pd.concat([history,dqs])
               
     #respondents = history[history['studyidguid']==studyid.lower()]
-    return history[history.name == 'Unity Frame Shopping'], dqs[dqs.name== 'Unity Frame Shopping']
+    return history[history.name == 'Unity Frame Shopping'], dqs[dqs.name== 'Unity Frame Shopping'], rawShopHistory
     
 def to_excel(data,filename):
     writer = pd.ExcelWriter(filename, engine='xlsxwriter')
@@ -48,12 +49,8 @@ def to_excel(data,filename):
 def calc_and_plot(outliers_fraction):
     
     # Example settings
-    data, dqs = get_data()
+    data, dqs, raw = get_data()
     n_samples = data.shape[0]
-    print n_samples
-    
-    
-
     
     # define two outlier detection tools to be compared
     '''
@@ -106,11 +103,14 @@ def calc_and_plot(outliers_fraction):
                      colors='orange')
     b = plt.scatter(data['bandwidth'],data['latency'], c='white')
     c = plt.scatter(dqs['bandwidth'],dqs['latency'], c='black')
+    
+    #picking "bad" outliers, not good ones
+    outliers = outliers[outliers['bandwidth']<outliers['latency']]
     outies = plt.scatter(outliers['bandwidth'],outliers['latency'], s=100, facecolors='none', edgecolors='g')
     plt.axis('tight')
     plt.legend(
-        [a.collections[0], b, c],
-        ['learned decision function', 'true inliers', 'true outliers'],
+        [a.collections[0], b, c, outies],
+        ['learned decision function', 'true inliers', 'true outliers', 'Recommended for Removal'],
         prop=matplotlib.font_manager.FontProperties(size=11))
     #subplot.set_xlabel("%d. %s (errors: %d)" % (i + 1, clf_name, n_errors))
     plt.xlim((-5, 5))
@@ -118,10 +118,51 @@ def calc_and_plot(outliers_fraction):
     #plt.subplots_adjust(0.04, 0.1, 0.96, 0.94, 0.1, 0.26)
     
     plt.show()
+    
+    outliers = raw[raw['ticketid'].isin(outliers['ticketid'])]
     return outliers
+    
+    
+    
+    
+def plot3d():
+    outliers_fraction = .1
+    data, dqs = get_data()
+    n_samples = data.shape[0]
+    clf = EllipticEnvelope(contamination=.1)
+    
+    #xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-3, 3, 500))
+    n_inliers = int((1. - outliers_fraction) * n_samples)
+    n_outliers = int(outliers_fraction * n_samples)
+    ground_truth = np.ones(n_samples, dtype=int)
+    ground_truth[-n_outliers:] = 0
+    
+    
+    
+    
+    X = zip(data['bandwidth'],data['latency'],data['framerate'])
+    
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    clf.fit(zip(data['bandwidth'],data['latency'],data['framerate']))
+    xx, yy = np.meshgrid(np.linspace(data['bandwidth'].min(), data['bandwidth'].max(), 500), np.linspace(data['latency'].min(), data['latency'].max(), 500))
+    zz = yy-xx
+    ax.scatter(data['bandwidth'],data['latency'],data['framerate'], c='b', marker='o')
+    ax.scatter(dqs['bandwidth'],dqs['latency'],dqs['framerate'], c='r', marker='^')
+    ax.hold(True)
+
+    ax.plot_surface(xx,yy,zz,color='orange',linewidth=0)
+    ax.set_xlabel('bandwidth')
+    ax.set_ylabel('latency')
+    ax.set_zlabel('framerate')
     
 
     
+    ax.view_init(elev=0., azim=90)        
+    
+    plt.show()
+
+#plot3d()    
     
 data = calc_and_plot(outliers_fraction = 0.10)
 to_excel(data,'mahalTest.xlsx')
