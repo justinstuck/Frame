0be8f06b47799ae44e4305c0ae94624ed0143df7
justinstuck <justin.stuck@incontextsolutions.com>
2016-07-14 10:09:49 -0500

    Removed loop to handle multiple clusters
    There are not multiple clusters so this is unnecessary. Kernel svm
    should be removed next

---
+++ frame_analysis.py
@@ -0,0 +1,179 @@
+from StringIO import StringIO
+from requests import get
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import statsmodels.api as sm
+import itertools
+import math
+import scipy.stats as stats
+from cStringIO import StringIO
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+
+
+class FrameCleaner():
+    def __init__(self,studyid):
+        self.studyid = studyid
+        self.features = [ 'bandwidth', 'latency', 'framerate']
+        self.experiences = ['Shopping', 'Tutorial']
+        self.charts = StringIO()
+    def Normalize(self, data):
+        features = self.features
+        sc = StandardScaler()
+        normData = {x:sc.fit_transform(np.array(data[x])) for x in features}
+        return pd.DataFrame(normData,index=data.ticketid)
+
+    def calc_stats(self, data):
+        features = self.features
+        d = {'mean' : pd.Series([data[x].mean() for x in features], index=features),
+             'std' : pd.Series([data[x].std() for x in features], index=features),
+             'Q1' : pd.Series([np.percentile(data[x],25) for x in features], index=features),
+             'Q3' : pd.Series([np.percentile(data[x],75) for x in features], index=features)}
+        self.stats = pd.DataFrame(d,index=features).transpose()
+        return self.stats
+        
+    def plot_histos(self,features):
+        if features != None:
+            feats = features
+        else:
+            feats = self.features
+        fig = plt.figure()
+        fig = plt.gcf()
+        fig.set_size_inches(10.5, 10.5)
+        #fig.savefig('test2png.png', dpi=100)
+        for feature in feats:
+            datashop =self.shopping[feature]
+            datatut = self.tutorial[feature]
+            if feature == 'framerate':
+                bins = np.linspace(datashop[datashop>0].min(),datashop[datashop>0].max(),26)
+            else:
+                bins = np.linspace(datashop[datashop>0].min(),datashop[datashop>0].max(),40)
+                #bins = np.linspace(datashop[datashop>0].min(),200,50)
+            plt.subplot(len(feats), 1, feats.index(feature)+1)
+            plt.hist(datashop, bins, alpha=0.8, label='Shopping')
+            plt.hist(datatut, bins, alpha=0.8, label='Tutorial', color='crimson')
+            #fit_alpha, fit_loc, fit_beta=stats.gamma.fit(data.shopping)
+            plt.legend(loc='upper left', frameon=False)
+            plt.xlabel(feature)
+        plt.tight_layout()
+        fig.savefig(self.charts)
+        
+    def get_data(self):
+        uri = 'http://ds/api/warehouse/all-the-frame-metrics-by-step?fmt=csv'
+        resp = get(uri)
+        self.history = pd.read_csv(StringIO(resp.text))
+        
+        self.dqs = pd.read_csv("C:/Users/Justin.Stuck/Desktop/JDQs.csv",low_memory=False)['ticketId']
+        
+        self.history['latency'] = self.history['latency'].apply(lambda x: math.log(x+.0000000000001))
+        self.history['bandwidth'] = self.history['bandwidth'].apply(lambda x: math.log(x+.0000000000001))
+        self.history = pd.concat([self.history[self.history.name =='Unity Frame Shopping'],self.history[self.history.name =='Unity Frame Tutorial']])
+        self.full_history=self.history
+        
+        
+        self.dqs = self.history[self.history['ticketid'].isin(self.dqs)]
+        self.dqs = self.dqs[self.dqs.name == 'Unity Frame Shopping']        
+        
+        
+        self.history = self.history[self.history.iscomplete == 1]
+        self.respondents = self.history[self.history['studyidguid']==self.studyid.lower()]
+        self.shopping = self.respondents[self.respondents.name == 'Unity Frame Shopping']
+        self.tutorial = self.respondents[self.respondents.name == 'Unity Frame Tutorial']
+        self.tutorial = self.tutorial[self.tutorial.ticketid.isin(self.shopping.ticketid.values)]
+        
+    def tukey_filter(self, data):
+        q1=np.array([self.stats[feat]['Q1'] for feat in self.features])
+        q3=np.array([self.stats[feat]['Q3'] for feat in self.features])
+        iqr = q3-q1
+        lowFences = dict(zip(self.features,q1-1.5*iqr))
+        highFences = dict(zip(self.features,q3+1.5*iqr))
+        print lowFences
+        print highFences
+        self.highLat = data[data['latency']>highFences['latency']]   
+        self.lowBand = data[data['bandwidth']<lowFences['bandwidth']]
+        self.lowFR = data[data['framerate']<self.stats['framerate']['mean']-2*self.stats['framerate']['std']]
+        self.highLat['reason'] = 'High Latency'
+        self.lowBand['reason'] = 'Low Bandwidth'
+        self.lowFR['reason'] = 'Low Frame Rate'
+        
+        return pd.concat([self.highLat,self.lowBand,self.lowFR])
+        
+        
+        
+        
+        
+        
+        
+    def logistic_filter(self):
+        X, y = self.history[self.features], self.respondents.iscomplete
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y,test_size=0.05,random_state=0)
+
+        sc = StandardScaler()
+        sc.fit(self.X_train)
+        self.X_train_std = sc.transform(self.X_train)
+        self.X_test_std = sc.transform(self.X_test)
+        x = self.respondents['latency']
+        y = self.respondents['bandwidth']
+        z = self.respondents['framerate']
+        
+        
+        
+        
+        
+        
+        
+        
+    def to_excel(self, removals, filename):
+        writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+        
+        removals.to_excel(writer, sheet_name='Removal Candidates')
+        
+        writer.save()
+
+
+
+
+
+
+#fc = FrameCleaner('04CE2574-357E-491A-9BD1-51D0A45F32ED')
+fc = FrameCleaner('4BC154FD-6429-444E-B589-4B3B7CF1BDA6')
+fc.get_data()
+#fc.get_removals()
+
+completes = fc.shopping[fc.shopping.iscomplete == 1]
+incompletes = fc.shopping[fc.shopping.iscomplete == 0]
+
+#fc.calc_stats(fc.respondents)
+data = fc.calc_stats(fc.history)
+
+fc.plot_histos(features=fc.features)
+fc.to_excel(fc.tukey_filter(fc.shopping),'frameremovals.xlsx')
+
+print fc.stats
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ outlierDetectionExample.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 09:27:22 2016
+
+@author: Justin.Stuck
+"""
+
+print(__doc__)
+import pandas as pd
+from StringIO import StringIO
+from requests import get
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from scipy import stats
+
+from sklearn import svm
+from sklearn.covariance import EllipticEnvelope
+
+from sklearn import preprocessing
+
+
+def get_data():
+    uri = 'http://ds/api/warehouse/all-the-frame-metrics-by-step?fmt=csv'
+    resp = get(uri)
+    history = pd.read_csv(StringIO(resp.text))
+    study = '4BC154FD-6429-444E-B589-4B3B7CF1BDA6'
+    #'4BC154FD-6429-444E-B589-4B3B7CF1BDA6'
+    dqs = pd.read_csv("C:/Users/Justin.Stuck/Desktop/JDQs.csv",low_memory=False)['ticketId']
+    history = pd.concat([history[history.name =='Unity Frame Shopping'],history[history.name =='Unity Frame Tutorial']])
+    history['latency'] = preprocessing.scale(history['latency'].apply(lambda x: math.log(x+.0000000000001)))
+    history['bandwidth'] = preprocessing.scale(history['bandwidth'].apply(lambda x: math.log(x+.0000000000001)))
+    history['framerate'] = preprocessing.scale(history['framerate'])
+    dqs = history[history['ticketid'].isin(dqs)]   
+    history = history[history['studyidguid']==str.lower(study)]
+    history = history[history.iscomplete == 1]
+    
+    history = pd.concat([history,dqs])
+              
+    #respondents = history[history['studyidguid']==studyid.lower()]
+    return history[history.name == 'Unity Frame Shopping'], dqs[dqs.name== 'Unity Frame Shopping']
+    
+        
+
+
+# Example settings
+data, dqs = get_data()
+n_samples = data.shape[0]
+print n_samples
+
+outliers_fraction = 0.10
+clusters_separation = [0]
+
+# define two outlier detection tools to be compared
+classifiers = {
+    "One-Class SVM": svm.OneClassSVM(nu=0.95 * outliers_fraction + 0.05,
+                                     kernel="rbf", gamma=0.1),
+    "robust covariance estimator": EllipticEnvelope(contamination=.01)}
+
+
+xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-3, 3, 500))
+n_inliers = int((1. - outliers_fraction) * n_samples)
+n_outliers = int(outliers_fraction * n_samples)
+ground_truth = np.ones(n_samples, dtype=int)
+ground_truth[-n_outliers:] = 0
+
+X = zip(data['bandwidth'],data['latency'])
+#plt.scatter(X[:,0],X[:,1])
+#print X
+plt.show()
+plt.figure(figsize=(10, 5))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    # fit the data and tag outliers
+    clf.fit(zip(data['bandwidth'],data['latency']))
+    data['y_pred'] = clf.decision_function(X).ravel()
+    threshold = stats.scoreatpercentile(data['y_pred'],
+                                        100 * outliers_fraction)
+    data['y_pred'] = data['y_pred'] > threshold
+    n_errors = (data['y_pred'] != ground_truth).sum()
+    # plot the levels lines and the points
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    subplot = plt.subplot(1, 2, i + 1)
+    subplot.set_title("Outlier detection")
+    subplot.set_xlabel('bandwidth')
+    subplot.set_ylabel('latency')
+    
+    subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
+                     cmap=plt.cm.Blues_r)
+    a = subplot.contour(xx, yy, Z, levels=[threshold],
+                        linewidths=2, colors='red')
+    subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
+                     colors='orange')
+    b = subplot.scatter(data['bandwidth'],data['latency'], c='white')
+    c = subplot.scatter(dqs['bandwidth'],dqs['latency'], c='black')
+    subplot.axis('tight')
+    subplot.legend(
+        [a.collections[0], b''', c'''],
+        ['learned decision function', 'true inliers', 'true outliers'],
+        prop=matplotlib.font_manager.FontProperties(size=11))
+    #subplot.set_xlabel("%d. %s (errors: %d)" % (i + 1, clf_name, n_errors))
+    subplot.set_xlim((-5, 5))
+    subplot.set_ylim((-3, 3))
+plt.subplots_adjust(0.04, 0.1, 0.96, 0.94, 0.1, 0.26)
+
+plt.show()
